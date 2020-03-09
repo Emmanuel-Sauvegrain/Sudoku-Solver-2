c0c9ea3b335bdb981ea86b224b6591d0de3bebdf
Emmanuel <emmanuelsauvegrain@hotmail.com>
2020-03-09 13:57:28 -0400

    Update Main.java

--- src/App/Main.java
+++ src/App/Main.java
@@ -19,36 +19,37 @@ public class Main {
 			
 			if(args[i].contentEquals("mrv")){
 				mrv=true;
-				System.out.println("MRV");
 			}
 			
 			if(args[i].contentEquals("dh")){
 				dh=true;
-				System.out.println("DH");
 			}
 			
 			if(args[i].contentEquals("lcv")){
 				lcv=true;
-				System.out.println("LCV");
 			}
 		
 			if(args[i].contentEquals("ac3")){
 				ac3=true;
-				System.out.println("AC3");
 			}
 			
 			if(args[i].indexOf("load")==0 && !changed) {
 			
 				String[] arrTemp = args[i].split("=",2);
-				System.out.println(arrTemp[1]);
 				try {
 					sudoku.loadGrid(arrTemp[1]);
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				changed = true;	
 			}
+			
+			if(args[i].contentEquals("generate") && !changed) {
+				Generator generator = new Generator();
+				sudoku.changeGrid(generator.generateGrid());
+				changed = true;
+			}
+
 		}
 		//Initialisation et lancement du Solver, selon les arguments donnés, et affichage du résultat
 		long start = System.currentTimeMillis();
@@ -63,8 +64,7 @@ public class Main {
 			System.out.println();
 			System.out.println("Resolution failed");
 		}
-		//Generator generator = new Generator();
-		//sudoku.changeGrid(generator.generateGrid());
+		
 	}
 
 }
